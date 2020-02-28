7a87d02ee537d4ca8c246b11dbfe3a6252a5371f
Resssland <Ruslan_starodub@bk.ru>
2020-02-29 09:14:10 +1000

    Fix brt trace searching command

--- src/main/java/Frames/SearchTraceFrame.java
+++ src/main/java/Frames/SearchTraceFrame.java
@@ -196,7 +196,7 @@ public class SearchTraceFrame {
 
 
         private void GetTraceSessions(Config config){
-            String command="find /data/brt/BRT/current/logs/ -mindepth 1  -newermt '"+TimeConvertor.CalendarToString(config.getDateFrome())+"' ! -newermt '"+TimeConvertor.CalendarToString(config.getDateTo())+"' -exec zgrep '= "+config.getMsisdn()+"' {} \\; | grep hrs";
+            String command="find /data/brt/BRT/current/logs/ -mindepth 1  -newermt '"+TimeConvertor.CalendarToString(config.getDateFrome())+"' ! -newermt '"+TimeConvertor.CalendarToString(config.getDateTo())+"' -exec zgrep '= "+config.getMsisdn()+"' {} \\; | grep -E 'hrs.*msisdn'";
             Logger.log("Command of searching sessions:"+command,null);
             //StartButton.setEnabled(false);
             ArrayList<String> rowSessions=new ArrayList<String>();
